eafb1f09e7318137ee1259d44f4e91a89578de1f
josenauto <jemilionautoch@gmail.com>
2020-12-27 21:54:57 -0300

    Navigated questions and showed the content of the question component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,18 @@ import data from "./data";
 import SingleQuestion from "./Question";
 
 function App() {
+  const [questions, setQuestions] = useState(data);
   return (
-    <div>
-      <h2>accordion project setup</h2>
-    </div>
+    <main>
+      <div className="container">
+        <h3>questions and answers about login</h3>
+        <section className="info">
+          {questions.map((question) => {
+            return <SingleQuestion key={question.id} {...question} />;
+          })}
+        </section>
+      </div>
+    </main>
   );
 }
 
