c66e874be40cedf88cb12e16ba6e0d0215285590
josenauto <jemilionautoch@gmail.com>
2020-12-28 18:57:25 -0300

    Installed react-bootstrap and added some components

--- src/Question.js
+++ src/Question.js
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { Card, Button } from "react-bootstrap";
 
 const Question = ({ title, info }) => {
+  const [showInfo, setShowInfo] = useState(false);
   return (
-    <article className="question">
-      <header>
-        <h4>{title}</h4>
-        <button className="btn">btn</button>
-      </header>
-      <p>{info}</p>
-    </article>
+    // <article className="question">
+    //   <header>
+    //     <h4>{title}</h4>
+    //     <button className="btn">btn</button>
+    //   </header>
+    //   <p>{info}</p>
+    // </article>
+    <Card>
+      <Card.Header>
+        <Button variant="primary" onClick={() => setShowInfo(!showInfo)}>
+          &#x2795;
+        </Button>
+        {title}
+      </Card.Header>
+      {showInfo && (
+        <Card.Body>
+          <Card.Text>{info}</Card.Text>
+        </Card.Body>
+      )}
+    </Card>
   );
 };
 
